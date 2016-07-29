051d1d8bc4dd556f085c85b17d916eff962b1a52
Karuppasamy M <karuppasamy.m@cognizant.com>
2016-07-29 18:48:58 +0800

    Post creation added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,13 +5,25 @@ class PostsController < ApplicationController
     render json: @post
   end
 
+  def create
+    @post = Post.new(permitted_params)
+
+    respond_to do |format|
+      if @post.save
+        format.json { render json: { message: 'Success' } }
+      else
+        format.json { render json: @post.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   private
 
   def collect_post
     @post = Post.all
   end
 
-  def person_params
-    params.require(:post).permit(:image, :description, :amount_expected)
+  def permitted_params
+    params.permit(:image, :description, :amount_expected)
   end
 end
